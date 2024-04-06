f50c2f75b658a234b0f8e5a8603f7f07f3e527c3
Oumaimabben <obenothmen865@gmail.com>
2024-04-06 07:49:05 +0200

    complete signup page funcionallity

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //add the server when the api lanched  the target lanched first
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
