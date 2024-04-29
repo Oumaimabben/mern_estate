4ec96319af23ae75959ff93ea90b836c78db4bd5
Oumaimabben <obenothmen865@gmail.com>
2024-04-29 13:41:16 +0200

    complet USERRoute

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,14 +1,9 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 
-export const test = (req, res) => {
-  res.json({
-    message: 'Api route is working!',
-  });
-};
-//UPDATE
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only update your own account!'));
@@ -37,7 +32,7 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
-//DELETE
+
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only delete your own account!'));
@@ -50,8 +45,19 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
 
-//GET
 export const getUser = async (req, res, next) => {
   try {
     
