ffc82e6be06ba0d2247837de3cf319162f7c61b5
Oumaimabben <obenothmen865@gmail.com>
2024-05-03 19:55:50 +0200

    complet listing fonctionnality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -34,7 +34,7 @@ export default function CreateListing() {
   const [loading, setLoading] = useState(false);
   console.log(formData);
 
-  const handleImageSubmit = (e) => {
+  const handleImageSubmit = () => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
       setUploading(true);
       setImageUploadError(false);
@@ -52,7 +52,7 @@ export default function CreateListing() {
          setImageUploadError(false);
          setUploading(false);
         })
-       .catch((err) => {
+       .catch(() => {
           setImageUploadError('Image upload failed (2 mb max per image)');
           setUploading(false);
        });
